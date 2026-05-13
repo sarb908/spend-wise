ab56c90dcb77e6d1fcb8785985805fffc094a1c5
sarbjot-arnifi <sarbjot@arnifi.com>
2026-05-13 18:47:15 +0530

    Add User, Income, and Expense models with SQLAlchemy

---
+++ models/expense.py
@@ -0,0 +1,15 @@
+from db.database import Base
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import Integer, String, Date, Boolean, Float
+
+
+
+class DbExpense(Base):
+    __tablename__ = 'expenses'
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Float, nullable=False)
+    date = Column(Date, nullable=False)
+    category = Column(String, nullable=False)
+    is_recurring = Column(Boolean, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False, index=True)
---
+++ models/income.py
@@ -0,0 +1,17 @@
+from db.database import Base
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import Integer, String, Float, Date, Boolean
+from sqlalchemy.orm import relationship
+
+
+class DbIncome(Base):
+    __tablename__ = 'income'
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Float, nullable = False)
+    date = Column(Date, nullable = False)
+    source = Column(String, nullable = False)
+    is_recurring = Column(Boolean, nullable = False)
+    user_id = Column(Integer, ForeignKey("users.id") , nullable=False, index=True)
+
+    user = relationship("User", back_populates="expenses")
