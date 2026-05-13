6c15bf94724c6c79ac0911376bf9b18efe73c879
sarbjot-arnifi <sarbjot@arnifi.com>
2026-05-13 22:05:21 +0530

    Add authentication dependency for protected routes

---
+++ dependencies.py
@@ -0,0 +1,33 @@
+from jose import jwt,JWTError
+from fastapi import Depends,HTTPException,status
+from sqlalchemy.orm import Session
+from database import get_db
+from models.user import DbUser
+from auth_utils import SECRET_KEY,ALGORITHIM,oauth2_scheme
+
+
+
+def get_current_user(token:str = Depends(oauth2_scheme),db:Session = Depends(get_db)):
+
+
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Invalid Credentials',
+        headers={'WWW-Authenticate':'Bearer'}
+    )
+
+    try:
+        payload = jwt.decode(token,SECRET_KEY,algorithms=ALGORITHIM)
+        username : str =  payload.get("sub")
+
+        if not username:
+            raise credentials_exception
+
+    except JWTError:
+        raise credentials_exception
+    
+    user = db.query(DbUser).filter(DbUser.username == username).first()
+
+    if user is None:
+        raise credentials_exception
+    return user
